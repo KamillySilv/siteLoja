12bddad0095d3d688090947760efb6146f8ab61f
KamillySilv <kamillyvitoriapro71@gmail.com>
2023-04-23 20:59:44 -0300

    faculdade

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click",mobileMenu);
+
+function mobileMenu(){
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n=> n.addEventListener("click", mobileMenu));
+
+function closeMenu(){
+    hamburguer.classList.remove("active");
+    navMenu.classList.remove("active");
+    
+}
